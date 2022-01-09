c202f980d28cc59b1b2782a70cc0473bbd0de33e
Σοφία Κατσάκη <skatsaks@csd.auth.gr>
2022-01-09 20:24:06 +0200

    Changes/Additions: 1. CPU class: added methods: 1a)getCurrentProcess 1b)setCurrentProcess +addition:one simple inc of var clock 2.First Fit Class:FF implementation 3.Process Class: added: 3a)getMemoryRequirements +additions: methods: 3b)getWaitingTime 3c)getResponseTime 3d)getTurnAroundTime 4.ProcessControlBlock Class: added a DRAFT if in setState method in order for the state to change from NEW to READY

--- code/CPU.java
+++ code/CPU.java
@@ -1,29 +1,41 @@
 import java.util.ArrayList;
 
-public class CPU {
-
+public class CPU
+{
     public static int clock = 0; // this should be incremented on every CPU cycle
-    
     private Scheduler scheduler;
     private MMU mmu;
     private Process[] processes;
     private int currentProcess;
     
-    public CPU(Scheduler scheduler, MMU mmu, Process[] processes) {
+    public CPU(Scheduler scheduler, MMU mmu, Process[] processes)
+    {
         this.scheduler = scheduler;
         this.mmu = mmu;
         this.processes = processes;
     }
+
+    public void setCurrentProcess(int currentProcess)
+    {
+        this.currentProcess=currentProcess;
+    }
+
+    public int getCurrentProcess()
+    {
+        return this.currentProcess;
+    }
     
-    public void run() {
+    public void run()
+    {
         /* TODO: you need to add some code here
          * Hint: you need to run tick() in a loop, until there is nothing else to do... */
 
     }
     
-    public void tick() {
+    public void tick()
+    {
         /* TODO: you need to add some code here
          * Hint: this method should run once for every CPU cycle */
-        
+        clock+=1;
     }
 }
--- code/FirstFit.java
+++ code/FirstFit.java
@@ -1,19 +1,35 @@
 import java.util.ArrayList;
 
-public class FirstFit extends MemoryAllocationAlgorithm {
+public class FirstFit extends MemoryAllocationAlgorithm
+{
     
     public FirstFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
 
-    public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
+    public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots)
+    {
         boolean fit = false;
         int address = -1;
+
+        int counterOfPlaceInArraylistCurrenltyUsedMemorySlots=0;
+        for (MemorySlot memorySlot:currentlyUsedMemorySlots)
+        {
+
+            int sizeOfAvailableBlockOfSLot= memorySlot.getEnd()-memorySlot.getStart();
+            if ((sizeOfAvailableBlockOfSLot>=p.getMemoryRequirements()) && (p.getPCB().getState().equals(ProcessState.NEW)))
+            {
+                fit = true;
+                address = memorySlot.getStart();
+                currentlyUsedMemorySlots.get(counterOfPlaceInArraylistCurrenltyUsedMemorySlots).setStart(memorySlot.getStart()+p.getMemoryRequirements());
+                break;
+            }
+            counterOfPlaceInArraylistCurrenltyUsedMemorySlots++;
+        }
         /* TODO: you need to add some code here
          * Hint: this should return the memory address where the process was
          * loaded into if the process fits. In case the process doesn't fit, it
          * should return -1. */
-
         return address;
     }
 
