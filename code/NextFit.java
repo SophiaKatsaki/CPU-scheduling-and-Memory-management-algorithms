7c8fe79406204a6b9d5635c82dde4b079f1333a2
Σοφία Κατσάκη <skatsaks@csd.auth.gr>
2022-01-16 11:14:23 +0200

    Minor Changes in comments in First Fit and Next Fit algorithms.

--- code/NextFit.java
+++ code/NextFit.java
@@ -15,25 +15,27 @@ public class NextFit extends MemoryAllocationAlgorithm
         super(availableBlockSizes);
     }
 
-    /* This method decides whether a process fits in a block or not.If it fits,
-    Next Fit algorithm chooses the first available block that is greater than
-    or equal to the size of memory the process requires,after the last block that
-    a process was loaded. It can be considered as a rotating First Fit algorithm,beacuse
-    they both examine blocks in the same way,but usually with a different start block.
-    The address is by default set to -1, in case the process does not fit in any of the
-    blocks in the memory slots,and changes its value if the process does fit a block. The
-    address then,is set to the start adress of the certain available block.
-   */
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots)
     {
+
+       /* This method decides whether a process fits in a slot or not.If it fits,
+       Next Fit algorithm chooses the first available slot that is greater than
+       or equal to the size of memory the process requires,after the last slot that
+       a process was loaded. It can be considered as a rotating First Fit algorithm,beacuse
+       they both examine slots in the same way,but usually with a different start slot.
+       The address is by default set to -1, in case the process does not fit in any of the
+       slots in the memory slots,and changes its value if the process does fit a slot. The
+       address then,is set to the start address of the certain available slot.
+       */
+
         boolean fit = false;
         int address = -1;
         /* a counter that helps us not to execute the algorithm more times than the amount of the
-        available blocks. The algorithm exits the loop when all the blocks were checked for memory
+        available slots. The algorithm exits the loop when all the slots were checked for memory
         (in worst-case scenario).
         */
         int numberOfExecution=1;
-        /* We begin from the next block that is available after the last block that the previous
+        /* We begin from the next slot that is available after the last slot that the previous
         process was loaded*/
         int indexOfCurrentSlot=(last+1)%currentlyUsedMemorySlots.size();
         while(numberOfExecution<=currentlyUsedMemorySlots.size())
@@ -51,6 +53,7 @@ public class NextFit extends MemoryAllocationAlgorithm
             //updating the index before next execution
             indexOfCurrentSlot = (indexOfCurrentSlot + 1) % currentlyUsedMemorySlots.size();
 
+            //updating the number of times that Next Fit was executed
             numberOfExecution++;
 
         }
