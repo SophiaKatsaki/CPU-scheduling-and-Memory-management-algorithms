ca75ca11221efb269dd37da2c1d201a941aeea57
Σοφία Κατσάκη <skatsaks@csd.auth.gr>
2022-01-20 14:29:57 +0200

    Process Control Block Class: +assigning value to private final int "pid" variable +renaming the private int "currentTotalTimeRunBeforeSuspended" variable

--- code/ProcessControlBlock.java
+++ code/ProcessControlBlock.java
@@ -11,43 +11,43 @@ public class ProcessControlBlock
     
     private static int pidTotal= 0;
 
-    // Total runtime of the process until its last suspension (state changed from RUNNING to READY or TERMINATED).
-    private int currentTotalTimeRunBeforeSuspended;
-
+    // Current total runtime of the process.
+    private int currentTotalTimeRun;
 
     public ProcessControlBlock()
     {
         this.state = ProcessState.NEW;
         this.startTimes = new ArrayList<Integer>();
         this.stopTimes = new ArrayList<Integer>();
+
         //increasing the number of processes arrived by incrementing pidtotal
         pidTotal++;
+
         //the unique pid for every process is basically the order of its arrival
         this.pid = pidTotal;
-        currentTotalTimeRunBeforeSuspended =0;
+
+        currentTotalTimeRun =0;
     }
 
     public ProcessState getState() {
         return this.state;
     }
 
-
     public void setState(ProcessState state, int currentClockTime)
     {
         /* TODO: you need to add some code here
          * Hint: update this.state, but also include currentClockTime
          * in startTimes/stopTimes */
-
-        // Every time this process's execution is halted, the current total running time is computed and stored.
-        if (this.state==ProcessState.RUNNING && state!=ProcessState.RUNNING)
-        {
-            int previousHaltTime = stopTimes.isEmpty()?0:stopTimes.get(stopTimes.size()-1); // Get the timestamp of the previous halt.
-            currentClockTime += currentClockTime - previousHaltTime;
-        }
     }
 
-    public int getCurrentTotalTimeRunBeforeSuspended() {
-        return currentTotalTimeRunBeforeSuspended;
+    public int getCurrentTotalTimeRun()
+    {
+        return currentTotalTimeRun;
+    }
+
+    public void setCurrentTotalTimeRun(int currentTotalTimeRun)
+    {
+        this.currentTotalTimeRun = currentTotalTimeRun;
     }
 
     public int getPid() {
