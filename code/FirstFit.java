a5e65644718042df1205abf2d771f66f1eb30ebf
Σοφία Κατσάκη <skatsaks@csd.auth.gr>
2022-01-20 12:07:51 +0200

    Minor changes in First Fit and Next Fit comments.

--- code/FirstFit.java
+++ code/FirstFit.java
@@ -9,14 +9,10 @@ public class FirstFit extends MemoryAllocationAlgorithm
 
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots)
     {
-        /* This method decides whether a process fits in a slot or not.If it fits,
-        First Fit algorithm chooses the first available slot that is greater than
-        or equal to the size of memory the process requires. The address is by default
-        set to -1, in case the process does not fit in any of the slots in the memory
-        slots,and changes its value if the process does fit a slot. The address then,
-        is set to the start adress of the certain available slot.
-        */
         boolean fit = false;
+        /*The address is by default set to -1, in case the process does not fit
+        in any of the slots in the memory slots.
+        */
         int address = -1;
 
         int counterOfPlaceInArraylistCurrenltyUsedMemorySlots=0;
@@ -27,8 +23,10 @@ public class FirstFit extends MemoryAllocationAlgorithm
             if ((sizeOfAvailableBlockOfSLot>=p.getMemoryRequirements()))
             {
                 fit = true;
+                /*The address is set to the start adress of the certain
+                available slot.
+                */
                 address = memorySlot.getStart();
-                //currentlyUsedMemorySlots.get(counterOfPlaceInArraylistCurrenltyUsedMemorySlots).setStart(memorySlot.getStart()+p.getMemoryRequirements());
                 break;
             }
             counterOfPlaceInArraylistCurrenltyUsedMemorySlots++;
--- code/NextFit.java
+++ code/NextFit.java
@@ -18,34 +18,36 @@ public class NextFit extends MemoryAllocationAlgorithm
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots)
     {
 
-       /* This method decides whether a process fits in a slot or not.If it fits,
-       Next Fit algorithm chooses the first available slot that is greater than
-       or equal to the size of memory the process requires,after the last slot that
-       a process was loaded. It can be considered as a rotating First Fit algorithm,beacuse
-       they both examine slots in the same way,but usually with a different start slot.
+       /*
        The address is by default set to -1, in case the process does not fit in any of the
-       slots in the memory slots,and changes its value if the process does fit a slot. The
-       address then,is set to the start address of the certain available slot.
+       slots in the memory slots.
        */
 
         boolean fit = false;
         int address = -1;
+
         /* a counter that helps us not to execute the algorithm more times than the amount of the
         available slots. The algorithm exits the loop when all the slots were checked for memory
         (in worst-case scenario).
         */
         int numberOfExecution=1;
+
         /* We begin from the next slot that is available after the last slot that the previous
         process was loaded*/
         int indexOfCurrentSlot=(last+1)%currentlyUsedMemorySlots.size();
+
         while(numberOfExecution<=currentlyUsedMemorySlots.size())
         {
             int sizeOfAvailableBlockOfSLot = currentlyUsedMemorySlots.get(indexOfCurrentSlot).getEnd() - currentlyUsedMemorySlots.get(indexOfCurrentSlot).getStart();
             if (sizeOfAvailableBlockOfSLot >= p.getMemoryRequirements())
             {
                 fit = true;
+
+                /*The address is set to the start address of the certain
+                available slot.
+                */
                 address = currentlyUsedMemorySlots.get(indexOfCurrentSlot).getStart();
-                //currentlyUsedMemorySlots.get(indexOfCurrentSlot).setStart(currentlyUsedMemorySlots.get(indexOfCurrentSlot).getStart()+p.getMemoryRequirements());
+
                 last = indexOfCurrentSlot;
                 break;
             }
